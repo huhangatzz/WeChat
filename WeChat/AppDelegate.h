fdce826f9855aa405337bafeaec9624fdd3582f2
胡航 <1107313299@qq.com>
2016-09-22 19:18:24 +0800

    Initial Commit

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by huhang on 16/9/22.
+//  Copyright © 2016年 huhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by huhang on 16/9/22.
+//  Copyright © 2016年 huhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
